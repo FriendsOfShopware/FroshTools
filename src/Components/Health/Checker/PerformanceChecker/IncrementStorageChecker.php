b79c27185ba3f961e75a6c7fdbe46ab0fb01579c
Soner Sayakci <s.sayakci@shopware.com>
2022-04-24 19:44:41 +0000

    feat: add some performance checkers

---
+++ src/Components/Health/Checker/PerformanceChecker/IncrementStorageChecker.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace Frosh\Tools\Components\Health\Checker\PerformanceChecker;
+
+use Frosh\Tools\Components\Health\Checker\CheckerInterface;
+use Frosh\Tools\Components\Health\HealthCollection;
+use Frosh\Tools\Components\Health\SettingsResult;
+
+class IncrementStorageChecker implements CheckerInterface
+{
+    protected string $userActivity;
+    protected string $queueActivity;
+
+    public function __construct(string $userActivity, string $queueActivity)
+    {
+        $this->userActivity = $userActivity;
+        $this->queueActivity = $queueActivity;
+    }
+
+    public function collect(HealthCollection $collection): void
+    {
+        if ($this->userActivity === 'mysql' || $this->queueActivity === 'mysql') {
+            $collection->add(
+                SettingsResult::warning('frosh-tools.checker.incrementStorageIsDB',
+                    'mysql',
+                    'array or redis',
+                    'https://developer.shopware.com/docs/guides/hosting/performance/performance-tweaks#increment-storage'
+                )
+            );
+
+            return;
+        }
+
+        $collection->add(
+            SettingsResult::ok('frosh-tools.checker.incrementStorageIsNotDB',
+                $this->userActivity,
+                $this->userActivity
+            )
+        );
+    }
+}
