efc9fd1b16953eeb7bb5ddc9c0af6f7f04da1324
Shyim <s.sayakci@shopware.com>
2022-10-22 01:45:35 +0200

    try to retrigger IDE indexing

--- src/FroshTools.php
+++ src/FroshTools.php
@@ -11,6 +11,8 @@ use Frosh\Tools\DependencyInjection\SymfonyConfigCompilerPass;
 use Shopware\Core\Framework\Plugin;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
+
+
 if (file_exists($vendorPath = __DIR__ . '/../vendor/autoload.php')) {
     require_once $vendorPath;
 }
