2618975c6693b234ee47ed2ecb9b065f1f996667
Micha Hobert <m.hobert@shopware.com>
2023-09-20 10:31:51 +0200

    Add route, template and export (#218)

---
+++ src/Components/StateMachines/Mermaid.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace Frosh\Tools\Components\StateMachines;
+
+use Shopware\Core\System\StateMachine\StateMachineEntity;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+final class Mermaid implements ExportInterface
+{
+    private const DEFAULT_PATH = __DIR__ . '/../../Resources/views/administration/mermaid';
+
+    private readonly Environment $twig;
+
+    public function __construct()
+    {
+        $path = realpath(self::DEFAULT_PATH);
+        if (!\is_string($path)) {
+            throw new \RuntimeException('Could not find default path for PlantUML templates');
+        }
+
+        $this->twig = new Environment(new FilesystemLoader($path));
+    }
+
+    public function export(StateMachineEntity $stateMachine, string $title = ''): string
+    {
+        return $this->twig->render('state-machine.html.twig', [
+            'title' => $title,
+            'initialState' => $stateMachine->getInitialState(),
+            'states' => $stateMachine->getStates()?->getElements(),
+            'transitions' => $stateMachine->getTransitions()?->getElements(),
+        ]);
+    }
+}
