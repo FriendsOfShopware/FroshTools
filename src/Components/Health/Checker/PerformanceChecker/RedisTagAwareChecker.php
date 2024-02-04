43c8bd8c7992a60d3f1408d0335fbdb9aec0773e
tinect <s.koenig@tinect.de>
2024-02-04 08:25:12 +0100

    feat: recommend RedisTagAware from Shopware 6.5.8.0 (#242)
    * feat: recommend RedisTagAware from Shopware 6.5.8.0
    
    * feat: add warning if RedisTagAware is active below 6.5.8.0
    
    * chore: fix codestyle

--- src/Components/Health/Checker/PerformanceChecker/RedisTagAwareChecker.php
+++ src/Components/Health/Checker/PerformanceChecker/RedisTagAwareChecker.php
@@ -9,19 +9,44 @@ use Frosh\Tools\Components\CacheRegistry;
 use Frosh\Tools\Components\Health\Checker\CheckerInterface;
 use Frosh\Tools\Components\Health\HealthCollection;
 use Frosh\Tools\Components\Health\SettingsResult;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class RedisTagAwareChecker implements PerformanceCheckerInterface, CheckerInterface
 {
     public function __construct(
-        private readonly CacheRegistry $cacheRegistry
+        private readonly CacheRegistry $cacheRegistry,
+        #[Autowire('%kernel.shopware_version%')]
+        protected string $shopwareVersion
     ) {}
 
     public function collect(HealthCollection $collection): void
     {
         $httpCacheType = $this->cacheRegistry->get('cache.http')->getType();
 
-        if (!\str_starts_with($httpCacheType, CacheAdapter::TYPE_REDIS)
-            || \str_starts_with($httpCacheType, CacheAdapter::TYPE_REDIS_TAG_AWARE)) {
+        $redisActive = \str_starts_with($httpCacheType, CacheAdapter::TYPE_REDIS);
+
+        if (!$redisActive) {
+            return;
+        }
+
+        $redisTagAwareActive = \str_starts_with($httpCacheType, CacheAdapter::TYPE_REDIS_TAG_AWARE);
+        $redisTagAwareSupported = \version_compare('6.5.8.0', $this->shopwareVersion, '<=');
+
+        if ($redisTagAwareActive && !$redisTagAwareSupported) {
+            $collection->add(
+                SettingsResult::warning(
+                    'redis-tag-aware-unsupported',
+                    'Redis TagAware adapter has issues with your Shopware version',
+                    CacheAdapter::TYPE_REDIS_TAG_AWARE,
+                    CacheAdapter::TYPE_REDIS,
+                    'https://developer.shopware.com/docs/guides/hosting/performance/caches.html#example-replace-some-cache-with-redis'
+                )
+            );
+
+            return;
+        }
+
+        if ($redisTagAwareActive || !$redisTagAwareSupported) {
             return;
         }
 
