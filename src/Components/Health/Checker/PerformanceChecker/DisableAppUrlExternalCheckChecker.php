95cb5baf444623841ca48d9b591c9b7133ba3ec7
Marcus Müller <25648755+M-arcus@users.noreply.github.com>
2024-09-01 14:39:39 +0200

    Add "App URL external check" checker

---
+++ src/Components/Health/Checker/PerformanceChecker/DisableAppUrlExternalCheckChecker.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Frosh\Tools\Components\Health\Checker\PerformanceChecker;
+
+use Frosh\Tools\Components\Health\Checker\CheckerInterface;
+use Frosh\Tools\Components\Health\HealthCollection;
+use Frosh\Tools\Components\Health\SettingsResult;
+use Shopware\Core\DevOps\Environment\EnvironmentHelper;
+
+class DisableAppUrlExternalCheckChecker implements PerformanceCheckerInterface, CheckerInterface
+{
+    public function collect(HealthCollection $collection): void
+    {
+        $appUrlCheckDisabled = (bool) EnvironmentHelper::getVariable('APP_URL_CHECK_DISABLED', false);
+        if (!$appUrlCheckDisabled) {
+            $collection->add(
+                SettingsResult::warning(
+                    'app-url-check-disabled',
+                    'App URL external check',
+                    'enabled',
+                    'disabled',
+                    'https://developer.shopware.com/docs/guides/hosting/performance/performance-tweaks.html#disable-app-url-external-check',
+                ),
+            );
+        }
+    }
+}
