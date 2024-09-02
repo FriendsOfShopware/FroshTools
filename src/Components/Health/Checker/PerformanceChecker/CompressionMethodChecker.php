398ace5a22f7147662a79a5caa985a180ae2bf83
Marcus Müller <25648755+M-arcus@users.noreply.github.com>
2024-09-02 11:21:08 +0200

    Compression method check: Merge checks into one class

---
+++ src/Components/Health/Checker/PerformanceChecker/CompressionMethodChecker.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Frosh\Tools\Components\Health\Checker\PerformanceChecker;
+
+use Frosh\Tools\Components\Health\Checker\CheckerInterface;
+use Frosh\Tools\Components\Health\HealthCollection;
+use Frosh\Tools\Components\Health\SettingsResult;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+class CompressionMethodChecker implements PerformanceCheckerInterface, CheckerInterface
+{
+    public const DOCUMENTATION_URL = 'https://developer.shopware.com/docs/guides/hosting/performance/performance-tweaks.html#using-zstd-instead-of-gzip-for-compression';
+
+    public function __construct(
+        #[Autowire('%kernel.shopware_version%')]
+        public readonly string $shopwareVersion,
+        #[Autowire('%shopware.cache.cache_compression%')]
+        public readonly bool $cacheCompressionEnabled,
+        #[Autowire('%shopware.cache.cache_compression_method%')]
+        public readonly string $cacheCompressionMethod,
+        #[Autowire('%shopware.cart.compress%')]
+        public readonly bool $cartCompressionEnabled,
+        #[Autowire('%shopware.cart.compression_method%')]
+        public readonly string $cartCompressionMethod,
+    ) {}
+
+    public function collect(HealthCollection $collection): void
+    {
+        if (\version_compare('6.6.4.0', $this->shopwareVersion, '>')) {
+            return;
+        }
+
+        $this->checkCompression($collection, 'Cache', $this->cacheCompressionEnabled, $this->cacheCompressionMethod);
+        $this->checkCompression($collection, 'Cart', $this->cartCompressionEnabled, $this->cartCompressionMethod);
+    }
+
+    private function checkCompression(HealthCollection $collection, string $functionality, bool $enabled, string $method): void
+    {
+        if (!$enabled) {
+            $collection->add(
+                SettingsResult::warning(
+                    strtolower($functionality) . '-compress',
+                    $functionality . ' compression',
+                    'disabled',
+                    'enabled',
+                    self::DOCUMENTATION_URL,
+                ),
+            );
+
+            return;
+        }
+
+        if ($method === 'gzip') {
+            $collection->add(
+                SettingsResult::warning(
+                    strtolower($functionality) . '-compression-method',
+                    $functionality . ' compression method',
+                    'gzip',
+                    'zstd',
+                    self::DOCUMENTATION_URL,
+                ),
+            );
+
+            return;
+        }
+
+        if ($method === 'zstd' && !extension_loaded('zstd')) {
+            $collection->add(
+                SettingsResult::error(
+                    strtolower($functionality) . '-compression-method-extension-zstd',
+                    'PHP extension zstd for ' . $functionality . ' compression method',
+                    'disabled',
+                    'enabled',
+                    self::DOCUMENTATION_URL,
+                ),
+            );
+        }
+    }
+}
