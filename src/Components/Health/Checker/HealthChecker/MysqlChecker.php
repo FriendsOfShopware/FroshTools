08c9ebc7e3a4451321717474bbf9857a0751c4ca
tinect <s.koenig@tinect.de>
2024-12-28 03:23:56 +0100

    feat: reuse versionMetaPos in MysqlChecker

--- src/Components/Health/Checker/HealthChecker/MysqlChecker.php
+++ src/Components/Health/Checker/HealthChecker/MysqlChecker.php
@@ -100,8 +100,9 @@ class MysqlChecker implements HealthCheckerInterface, CheckerInterface
     private function extract(string $versionString): array
     {
         if (mb_stripos($versionString, 'mariadb') === false) {
-            if (mb_strpos($versionString, '-')) {
-                $versionString = mb_substr($versionString, 0, mb_strpos($versionString, '-'));
+            $versionMetaPos = mb_strpos($versionString, '-');
+            if ($versionMetaPos) {
+                $versionString = mb_substr($versionString, 0, $versionMetaPos);
             }
 
             return ['mysql' => $versionString];
