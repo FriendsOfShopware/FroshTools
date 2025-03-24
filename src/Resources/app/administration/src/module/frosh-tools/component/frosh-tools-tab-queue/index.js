6038092b4a02e585be4662454a26316f52c9cc02
Benjamin Wittwer <benjamin.wittwer@a-k-f.de>
2025-03-24 16:23:36 +0100

    Unify snippets

--- src/Resources/app/administration/src/module/frosh-tools/component/frosh-tools-tab-queue/index.js
+++ src/Resources/app/administration/src/module/frosh-tools/component/frosh-tools-tab-queue/index.js
@@ -25,12 +25,12 @@ Component.register('frosh-tools-tab-queue', {
       return [
         {
           property: 'name',
-          label: 'Name',
+          label: 'frosh-tools.name',
           rawData: true,
         },
         {
           property: 'size',
-          label: 'Size',
+          label: 'frosh-tools.size',
           rawData: true,
         },
       ]
