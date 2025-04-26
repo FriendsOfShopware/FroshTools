98e8e036eaecafed30e8d915ccb7911793f776c6
tinect <s.koenig@tinect.de>
2025-04-26 19:31:28 +0200

    feat: add checks for PHP FPM

--- src/FroshTools.php
+++ src/FroshTools.php
@@ -21,6 +21,14 @@ class FroshTools extends Plugin
         $container->addCompilerPass(new DisableElasticsearchCompilerPass());
     }
 
+    public static function formatSize(float $size): string
+    {
+        $base = log($size) / log(1024);
+        $suffix = ['', 'k', 'M', 'G', 'T'][floor($base)];
+
+        return round(1024 ** ($base - floor($base)), 2) . $suffix;
+    }
+
     protected function createContainerExtension(): FroshToolsExtension
     {
         return new FroshToolsExtension();
