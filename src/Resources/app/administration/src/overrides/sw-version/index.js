aad1adca6a5fcb2f27e9869e96b1c31b0c81cde3
Soner Sayakci <s.sayakci@gmail.com>
2025-05-15 14:31:46 +0200

    fix: formatting issues

--- src/Resources/app/administration/src/overrides/sw-version/index.js
+++ src/Resources/app/administration/src/overrides/sw-version/index.js
@@ -1,86 +1,90 @@
-import template from './template.twig'
+import template from './template.twig';
 
-const { Component } = Shopware
+const { Component } = Shopware;
 
 Component.override('sw-version', {
-  template,
-  inject: ['froshToolsService', 'acl', 'loginService'],
+    template,
+    inject: ['froshToolsService', 'acl', 'loginService'],
 
-  async created() {
-    if (!this.checkPermission()) {
-      return
-    }
-
-    await this.checkHealth()
-  },
-
-  data() {
-    return {
-      health: null,
-      hasPermission: false,
-    }
-  },
-
-  computed: {
-    healthVariant() {
-      let variant = 'success'
-
-      for (let health of this.health) {
-        if (health.state === 'STATE_ERROR') {
-          variant = 'error'
-          continue
+    async created() {
+        if (!this.checkPermission()) {
+            return;
         }
 
-        if (health.state === 'STATE_WARNING' && variant === 'success') {
-          variant = 'warning'
-        }
-      }
-
-      return variant
+        await this.checkHealth();
     },
 
-    healthPlaceholder() {
-      let msg = 'Shop Status: Ok'
-
-      if (this.health === null) {
-        return msg
-      }
-
-      for (let health of this.health) {
-        if (health.state === 'STATE_ERROR') {
-          msg = 'Shop Status: May outage, Check System Status'
-          continue
-        }
-
-        if (health.state === 'STATE_WARNING' && msg === 'Shop Status: Ok') {
-          msg = 'Shop Status: Issues, Check System Status'
-        }
-      }
-
-      return msg
-    },
-  },
-
-  methods: {
-    async checkHealth() {
-      this.health = await this.froshToolsService.healthStatus(true)
-
-      this.checkInterval = setInterval(async () => {
-        try {
-          this.health = await this.froshToolsService.healthStatus(true)
-        } catch (e) {
-          console.error(e)
-          clearInterval(this.checkInterval)
-        }
-      }, 60000)
-
-      this.loginService.addOnLogoutListener(() =>
-        clearInterval(this.checkInterval)
-      )
+    data() {
+        return {
+            health: null,
+            hasPermission: false,
+        };
     },
 
-    checkPermission() {
-      return (this.hasPermission = this.acl.can('frosh_tools:read'))
+    computed: {
+        healthVariant() {
+            let variant = 'success';
+
+            for (let health of this.health) {
+                if (health.state === 'STATE_ERROR') {
+                    variant = 'error';
+                    continue;
+                }
+
+                if (health.state === 'STATE_WARNING' && variant === 'success') {
+                    variant = 'warning';
+                }
+            }
+
+            return variant;
+        },
+
+        healthPlaceholder() {
+            let msg = 'Shop Status: Ok';
+
+            if (this.health === null) {
+                return msg;
+            }
+
+            for (let health of this.health) {
+                if (health.state === 'STATE_ERROR') {
+                    msg = 'Shop Status: May outage, Check System Status';
+                    continue;
+                }
+
+                if (
+                    health.state === 'STATE_WARNING' &&
+                    msg === 'Shop Status: Ok'
+                ) {
+                    msg = 'Shop Status: Issues, Check System Status';
+                }
+            }
+
+            return msg;
+        },
     },
-  },
-})
+
+    methods: {
+        async checkHealth() {
+            this.health = await this.froshToolsService.healthStatus(true);
+
+            this.checkInterval = setInterval(async () => {
+                try {
+                    this.health =
+                        await this.froshToolsService.healthStatus(true);
+                } catch (e) {
+                    console.error(e);
+                    clearInterval(this.checkInterval);
+                }
+            }, 60000);
+
+            this.loginService.addOnLogoutListener(() =>
+                clearInterval(this.checkInterval)
+            );
+        },
+
+        checkPermission() {
+            return (this.hasPermission = this.acl.can('frosh_tools:read'));
+        },
+    },
+});
