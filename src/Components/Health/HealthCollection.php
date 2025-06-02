9b3215e6dc1a522acfbb45339f1776eecf1c731c
Felix Schneider <felix.schneider@wimmelbach.de>
2025-06-02 11:53:13 +0200

    feat: sort health collection by state

--- src/Components/Health/HealthCollection.php
+++ src/Components/Health/HealthCollection.php
@@ -11,6 +11,20 @@ use Shopware\Core\Framework\Struct\Collection;
  */
 class HealthCollection extends Collection
 {
+    private const STATE_PRIORITY = [
+        SettingsResult::ERROR => 0,
+        SettingsResult::WARNING => 1,
+        SettingsResult::INFO => 2,
+        SettingsResult::GREEN => 3,
+    ];
+
+    public function sortByState(): void
+    {
+        $this->sort(static function (SettingsResult $a, SettingsResult $b) {
+            return (self::STATE_PRIORITY[$a->state] ?? -1) <=> (self::STATE_PRIORITY[$b->state] ?? -1);
+        });
+    }
+
     protected function getExpectedClass(): ?string
     {
         return SettingsResult::class;
