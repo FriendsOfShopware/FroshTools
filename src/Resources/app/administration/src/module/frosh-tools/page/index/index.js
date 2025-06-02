c41212c7752df1136c9f9606c0176f5b5fcffb9b
Felix Schneider <felix.schneider@wimmelbach.de>
2025-06-02 16:25:38 +0200

    fix: switch to deprecated components for 6.7

--- src/Resources/app/administration/src/module/frosh-tools/page/index/index.js
+++ src/Resources/app/administration/src/module/frosh-tools/page/index/index.js
@@ -4,20 +4,20 @@ import template from './template.twig';
 const { Component } = Shopware;
 
 Component.register('frosh-tools-index', {
-  template,
-  computed: {
-    elasticsearchAvailable() {
-      if (Shopware.Store && Shopware.Store.get('context')) {
-        return (
-          Shopware.Store.get('context').app.config.settings
-            ?.elasticsearchEnabled || false
-        )
-      } else {
-        return (
-          Shopware.State.get('context').app.config.settings
-            ?.elasticsearchEnabled || false
-        )
-      }
+    template,
+    computed: {
+        elasticsearchAvailable() {
+            if (Shopware.Store && Shopware.Store.get('context')) {
+                return (
+                    Shopware.Store.get('context').app.config.settings
+                        ?.elasticsearchEnabled || false
+                );
+            } else {
+                return (
+                    Shopware.State.get('context').app.config.settings
+                        ?.elasticsearchEnabled || false
+                );
+            }
+        },
     },
-  },
-})
+});
