ba0c96c64dfcd63562e4f966f46b436a5ebc45c4
Felix Schneider <felix.schneider@wimmelbach.de>
2025-06-04 18:26:23 +0200

    fix: handle admin state access correctly

--- src/Resources/app/administration/src/module/frosh-tools/page/index/index.js
+++ src/Resources/app/administration/src/module/frosh-tools/page/index/index.js
@@ -1,23 +1,24 @@
 import './frosh-tools.scss';
 import template from './template.twig';
 
-const { Component } = Shopware;
+const {Component} = Shopware;
 
 Component.register('frosh-tools-index', {
-    template,
-    computed: {
-        elasticsearchAvailable() {
-            if (Shopware.Store && Shopware.Store.get('context')) {
-                return (
-                    Shopware.Store.get('context').app.config.settings
-                        ?.elasticsearchEnabled || false
-                );
-            } else {
-                return (
-                    Shopware.State.get('context').app.config.settings
-                        ?.elasticsearchEnabled || false
-                );
-            }
-        },
+  template,
+  computed: {
+    elasticsearchAvailable() {
+      try {
+        return (
+          Shopware.Store.get('context').app.config.settings
+            ?.elasticsearchEnabled || false
+        );
+
+      } catch {
+        return (
+          Shopware.State.get('context').app.config.settings
+            ?.elasticsearchEnabled || false
+        );
+      }
     },
+  },
 });
