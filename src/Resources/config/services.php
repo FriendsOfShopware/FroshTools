300f9ac295fee74442ba33cc47a96b0dd8b16571
Soner Sayakci <s.sayakci@gmail.com>
2026-02-10 14:37:34 +0100

    fix: switch to php cfg

---
+++ src/Resources/config/services.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $container): void {
+    $services = $container->services()
+        ->defaults()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->load('Frosh\Tools\\', '../../')
+        ->exclude('../../{DependencyInjection,Resources,FroshTools.php}');
+};
